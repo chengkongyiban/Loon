ea0b97b9acba82c90697797da84b35aabe714ddb
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-01-03 21:27:00 +0800

    更新 Surge_to_Loon.js

--- js/Surge_to_Loon.js
+++ js/Surge_to_Loon.js
@@ -51,22 +51,22 @@ body.forEach((x, y, z) => {
 				
 				let sctype = x.match('http-response') ? 'response' : 'request';
 				
-				let rebody = x.match('requires-body=(true|1)') ? ',requires-body=true' : '';
+				let rebody = x.match('requires-body=(true|1)') ? ', requires-body=true' : '';
 				
-				let proto = x.match('binary-body-mode=(true|1)') ? ',binary-body-mode=true' : '';
+				let proto = x.match('binary-body-mode=(true|1)') ? ', binary-body-mode=true' : '';
 				
 				let scname = x.replace(/\x20/gi,'').split("=")[0].replace(/#/,'');
 				
-				let ptn = x.replace(/\s/gi,"").split("pattern=")[1].split(",")[0].replace(/\"/gi,'');
+				let ptn = x.replace(/\x20/gi,"").split("pattern=")[1].split(",")[0].replace(/\"/gi,'');
 				
-				let js = x.replace(/\s/gi,"").split("script-path=")[1].split(",")[0];
+				let js = x.replace(/\x20/gi,"").split("script-path=")[1].split(",")[0];
 				
 				
 				
 				script.push(
 					x.replace(
 						/[^\s]+http-re[^\s]+/,
-						`${noteK}http-${sctype} ${ptn} script-path=${js}${rebody}${proto},tag=${scname}`
+						`${noteK}http-${sctype} ${ptn} script-path=${js}${rebody}${proto}, tag=${scname}`
 					),
 				);
 				}if (x.match(/http-(response|request)\x20/)){
@@ -75,12 +75,12 @@ body.forEach((x, y, z) => {
 					x = x.replace(/(\{.*?)\,(.*?\})/gi,'$1t&zd;$2');
 					
 				z[y - 1]?.match("#") && script.push(z[y - 1]);
-				let proto = x.match('binary-body-mode=(true|1)') ? ',binary-body-mode=true' : '';
-				let rebody = x.match('requires-body=(true|1)') ? ',requires-body=true' : '';
+				let proto = x.match('binary-body-mode=(true|1)') ? ', binary-body-mode=true' : '';
+				let rebody = x.match('requires-body=(true|1)') ? ', requires-body=true' : '';
 				
 				let ptn = x.split(" ")[1].replace(/\"/gi,'');
 				
-				let js = x.replace(/\s/gi,"").split("script-path=")[1].split(",")[0];
+				let js = x.replace(/\x20/gi,"").split("script-path=")[1].split(",")[0];
 				
 				let sctype = x.match('http-response') ? 'response' : 'request';
 				
@@ -89,7 +89,7 @@ body.forEach((x, y, z) => {
 				script.push(
 					x.replace(
 						/.*http-(response|request)\x20.+/,
-						`${noteK}http-${sctype} ${ptn} script-path=${js}${rebody}${proto},tag=${scname}`
+						`${noteK}http-${sctype} ${ptn} script-path=${js}${rebody}${proto}, tag=${scname}`
 					),
 				);
 				}else{
@@ -102,14 +102,14 @@ body.forEach((x, y, z) => {
 			x = x.replace(/cronexp/gi,'cronexp');
 				let croName = x.split("=")[0].replace(/\x20/gi,"").replace(/#/,'')
 				
-				let cronJs = x.split("script-path=")[1].split(",")[0].replace(/\x20/gi,"")
+				let cronJs = x.replace(/\x20/gi,"").split("script-path=")[1].split(",")[0]
 				
-				let cronExp = x.replace(/(.+cronexpr?=.+)/,"$1,").replace(/.+cronexpr?=(.+\x20.+?),.*/,"$1")
+				let cronExp = x.replace(/(.+cronexpr?\x20?=\x20?.+)/,"$1,").replace(/.+cronexpr?\x20?=\x20?(.+\x20.+?),.*/,"$1")
 				
 				script.push(
 					x.replace(
 						/.+cronexp.+/,
-						`${noteK}cron "${cronExp}" script-path=${cronJs},timeout=60,tag=${croName}`,
+						`${noteK}cron "${cronExp}" script-path=${cronJs}, timeout=60, tag=${croName}`,
 					),
 				);
 				
@@ -192,7 +192,7 @@ providers.push(
 	);
 *****/
 
-script = (script[0] || '') && `[Script]\n${script.join("\n")}`;
+script = (script[0] || '') && `[Script]\n${script.join("\n\n")}`;
 
 URLRewrite = (URLRewrite[0] || '') && `[Rewrite]\n${URLRewrite.join("\n")}`;
 
@@ -206,8 +206,11 @@ MapLocal = (MapLocal[0] || '') && `[MapLocal]\n${MapLocal.join("\n")}`;
 ********/
 
 body = `${name}
+
 ${desc}
+
 ${icon}
+
 
 ${Rule}
 
@@ -216,9 +219,11 @@ ${URLRewrite}
 ${script}
 
 ${MITM}`
+		.replace(/#(.+)\n/g,'#$1')
 		.replace(/t&zd;/g,',')
 		.replace(/\n{2,}/g,'\n\n')
 		.replace(/"{2,}/g,'"')
+		.replace(/\x20{2,}/g,' ')
 
 
  $done({ response: { status: 200 ,body:body ,headers: {'Content-Type': 'text/plain; charset=utf-8'} } });
