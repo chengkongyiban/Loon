9ea99458723d0d9a18e0cbda9db3e8c7650fe47b
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-01-09 19:28:16 +0800

    更新 Surge_to_Loon.js

--- js/Surge_to_Loon.js
+++ js/Surge_to_Loon.js
@@ -7,6 +7,11 @@
 插件图标用的 @Keikinn 的 StickerOnScreen项目，感谢
 
 ***************************/
+const isStashiOS = 'undefined' !== typeof $environment && $environment['stash-version'] && ua.indexOf('Macintosh') === -1
+const isSurgeiOS = 'undefined' !== typeof $environment && $environment['surge-version'];
+const isShadowrocket = 'undefined' !== typeof $rocket;
+const isLooniOS = 'undefined' != typeof $loon && /iPhone/.test($loon);
+
 var name = "";
 var desc = "";
 let req = $request.url.replace(/sg$|sg\?.*/,'');
@@ -250,9 +255,12 @@ ${icon}
 
 ${Rule}
 
+
 ${URLRewrite}
 
+
 ${script}
+
 
 ${MITM}`
 		.replace(/t&zd;/g,',')
@@ -261,7 +269,10 @@ ${MITM}`
 		.replace(/(#.+\n)\n/g,'$1')
 		.replace(/\n{2,}/g,'\n\n')
 
-others !="" && $notification.post("不支持的类型已跳过","第" + others,"点击查看原文，长按可展开查看跳过行",req)
+if (isSurgeiOS || isStashiOS) {
+           others !="" && $notification.post("不支持的类型已跳过","第" + others,"点击查看原文，长按可展开查看跳过行",{url:req});
+        } else {if (isLooniOS || isShadowrocket) {
+       others !="" && $notification.post("不支持的类型已跳过","第" + others,"点击查看原文，长按可展开查看跳过行",req);}};
 
  $done({ response: { status: 200 ,body:body ,headers: {'Content-Type': 'text/plain; charset=utf-8'} } });
 
