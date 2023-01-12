32770923a778ee26d05cbf39d990d20efc9c34ec
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-01-13 01:14:57 +0800

    更新 QX_to_Loon.js

--- js/QX_to_Loon.js
+++ js/QX_to_Loon.js
@@ -66,7 +66,7 @@ let others = [];//不支持的内容
 let MITM = "";
 
 body.forEach((x, y, z) => {
-	x = x.replace(/^(#|;|\/\/)/gi,'#');
+	x = x.replace(/^(#|;|\/\/)/gi,'#').replace(/\x20{2,}/g," ");
 	let type = x.match(
 		/\x20url\x20script-|enabled=|\x20url\x20reject|\x20echo-response|\-header|^hostname| url 30|\x20(request|response)-body/
 	)?.[0];
